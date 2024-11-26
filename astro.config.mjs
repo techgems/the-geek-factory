b3f6b16a1cd65235bb4fa26b4d3a54f4ec287a4f
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2024-11-26 10:40:50 -0600

    Add coming soon, remove unused pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: 'https://the-geek-factory.pages.dev/',
   integrations: [svelte(), tailwind()]
 });
