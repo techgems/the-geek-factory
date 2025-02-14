61ec2a582ddd24207737c616182221cc070c1e24
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2025-02-14 10:43:32 -0600

    Test images using Svelte.

---
+++ src/components/Test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { ImageMetadata } from 'astro';
+  const images = import.meta.glob<{ default: ImageMetadata }>(`/src/assets/pokemon-tcg/Supporter/*.png`, { eager: true });
+
+  const supporterUsedList = [
+    {
+        name: 'Acerola',
+        image: '/src/assets/pokemon-tcg/Supporter/Supporter Used - Acerola.png'
+    }
+];
+
+</script>
+
+<!-- ...existing code... -->
+
+{#each supporterUsedList as { name, image }, path}
+  <img src={images[image].default.src} alt={name} />
+{/each}
+
+<!-- ...existing code... -->
